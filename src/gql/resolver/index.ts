6e55110d5781a59f1efc1cce4717d074ba74112c
Hasan <sohagali.ru.ac@gmail.com>
2025-01-12 23:33:55 +0600

    add mutation

--- src/gql/resolver/index.ts
+++ src/gql/resolver/index.ts
@@ -1,3 +1,8 @@
 export const resolvers = {
   Query: {},
+  Mutation: {
+    userSignUp: (parent: any, args: any, context: any) => {
+      console.log(args);
+    },
+  },
 };
--- src/gql/schemas/index.ts
+++ src/gql/schemas/index.ts
@@ -1,18 +1,80 @@
 export const typeDefs = `#graphql
  type Query {
     user : User
+    admin : Admin
+    vendor : Vendor
+    customer : Customer
   }
 
+type Mutation {
+  userSignUp(input: UserSignUpInput!): User
+  createAdmin(input: CreateAdminInput!): Admin
+}
+
+input UserSignUpInput {
+  email: String!
+  password: String!
+  role: String!
+}
+
+input CreateAdminInput {
+  name: String!
+  email: String!
+  contactNumber: String!
+  emergencyContactNumber: String!
+  gender: String!
+  address: String!
+}
+
+type User {
+  id: ID!
+  email: String!
+  role: String!
+  createdAt: String!
+  updatedAt: String!
+}
+
+type Admin {
+  id: ID!
+  name: String!
+  email: String!
+  contactNumber: String!
+  emergencyContactNumber: String!
+  gender: String!
+  profileImg: String!
+  address: String!
+  isDeleted: Boolean!
+  createdAt: String!
+  updatedAt: String!
+}
+
+ type Vendor{
+    id :                    ID!
+    name :                  String!
+    email :                 String!
+    contactNumber :         String!
+    emergencyContactNumber: String!
+    gender :                String!
+    profileImg  :           String!
+    address     :           String!
+    isDeleted :             Boolean!
+    createdAt   :           String!
+    updatedAt  :            String!
+ }
 
 
-
- type User{
-    id        :    ID!
-    email     :    String!
-    role      :    String!
-    createdAt :    String!
-    updatedAt :    String!
- 
+ type Customer{
+    id :                    ID!
+    name :                  String!
+    email :                 String!
+    contactNumber :         String!
+    emergencyContactNumber: String!
+    gender :                String!
+    profileImg  :           String!
+    address     :           String!
+    isDeleted :             Boolean!
+    createdAt   :           String!
+    updatedAt  :            String!
  }
 
 
