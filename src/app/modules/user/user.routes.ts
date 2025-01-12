3bce7f1f532c145936395e06aafb2327ae3ccb36
Hasan <sohagali.ru.ac@gmail.com>
2025-01-12 20:43:45 +0600

    project setup with graphql

--- src/app/modules/user/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { UserController } from "./user.controller";
-
-const router = Router();
-
-router.post("/create-admin", UserController.createAdminIntoDB);
-
-export const UserRoutes = router;
